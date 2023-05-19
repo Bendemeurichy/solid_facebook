3804ca24ababa9890ec7394845b6d41f01cbe0b5
Ben De Meurichy <ben.demeurichy@gmail.com>
2023-05-19 11:13:40 +0200

    fixed bug

--- src/views/AddPost.vue
+++ src/views/AddPost.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+export type {Metadata};
 import { onMounted, ref } from "vue";
 import type { Contact } from "@/views/HomeView.vue";
 import { FOAF } from "@inrupt/vocab-common-rdf";
@@ -7,7 +8,7 @@ import {
  addUrl,
  createSolidDataset,
  createThing, getSolidDataset, getSourceUrl, getThingAll, getUrl,
- overwriteFile, saveSolidDatasetAt,
+ overwriteFile, saveSolidDatasetAt, setStringNoLocale,
  setThing, setUrl
 } from "@inrupt/solid-client";
 import { getDefaultSession } from "@inrupt/solid-client-authn-browser";
@@ -17,10 +18,12 @@ const postTitle = ref("");
 const postDescription = ref("");
 const postAuthor = ref("")
 const tagged = ref<Array<string>>([])
+const author = ref<string>("")
 
 const selectedFile = ref<File|null>(null);
 const imageUrl = ref<string | null>(null);
 let contacts = ref<Array<Contact>>([]);
+let authorlist = ref<Array<Contact>>([]);
 const session = getDefaultSession()
 const webId = session.info.webId;
 const fetch = session.fetch
@@ -31,6 +34,7 @@ function handleFileChange(event:Event) {
  if (target.files && target.files.length > 0) {
   selectedFile.value = target.files[0];
  }
+ uploadImage();
 }
 
 function uploadImage() {
@@ -133,6 +137,62 @@ async function getContactPublicName(profileUrl: string) {
  });
 };
 
+//GETNAME
+const getName = async () => {
+ // Initialize the Comunica engine
+ const source = webId
+ if (!source) return;
+ try{
+
+  // Define the SPARQL query to retrieve the foaf:name
+  const sparqlQuery = `
+    PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+    SELECT ?name
+    WHERE {
+      <${source}> foaf:name ?name .
+    }
+  `;
+  // Execute the query using the authorized fetch function
+  const bindingsStream = await engine.queryBindings(sparqlQuery, { sources: [source], fetch: fetch });
+
+  // Process the result to get the foaf:name
+  return new Promise((resolve, reject) => {
+   let name: string;
+   bindingsStream.on('data', (binding) => {
+    name = binding.get('name').value;
+   });
+   bindingsStream.on('end', () => resolve(name));
+   bindingsStream.on('error', (error) => reject(error));
+  });
+ } catch (e) {
+  console.error(e)
+ }}
+
+async function getProfilePicture() {
+ const source = webId
+ if (!source) return;
+ try{
+  const query = `
+      SELECT ?img
+      WHERE {
+        <${webId}> <http://xmlns.com/foaf/0.1/img> ?img .
+      }
+    `;
+  const bindingsStream = await engine.queryBindings(query, { sources: [source], fetch: fetch });
+
+  return new Promise((resolve, reject) => {
+   let profilePic: string;
+   bindingsStream.on('data', (binding) => {
+    profilePic = binding.get('img').value;
+   });
+   bindingsStream.on('end', () => resolve(profilePic));
+   bindingsStream.on('error', (error) => reject(error));
+  });
+ } catch (e) {
+  console.error(e)
+ };
+};
+
 type Metadata = {
   file: string,
  title: string,
@@ -142,34 +202,65 @@ type Metadata = {
 };
 
 //handling them files
-async function uploadMetadata(metadataUrl:string, metadata:Metadata) {
+async function uploadMetadata(metadataUrl: string, metadata: Metadata,imagetargetUrl:string) {
  // Create a new Thing for the metadata
  const metadataThing = createThing();
 
  // Add the metadata properties to the Thing
- const updatedMetadataThing = setUrl(setUrl(setUrl(setUrl(metadataThing,
-         FOAF.title, metadata.title),
-       FOAF.maker, metadata.author),
-     FOAF.depicts, metadata.tagged.join(',')),
-   FOAF.depiction, metadata.description);
+ const updatedMetadataThing = setUrl(setStringNoLocale(
+   setUrl(setStringNoLocale(metadataThing, FOAF.title, metadata.title), FOAF.maker, metadata.author),
+   FOAF.depiction, metadata.description),FOAF.img,imagetargetUrl);
+ const updatedMetadataThing1 = metadata.tagged.reduce((thing, tag) => addUrl(thing, FOAF.depicts, tag), updatedMetadataThing);
 
- // Create an empty RDF file
- let dataset = await getSolidDataset(metadataUrl, { fetch });
+ // Try to fetch the existing dataset, or create a new one if it doesn't exist
+ let dataset;
+ try {
+  dataset = await getSolidDataset(metadataUrl, { fetch });
+ } catch (error) {
+  if (error.statusCode === 404) {
+   dataset = createSolidDataset();
+  } else {
+   throw error;
+  }
+ }
 
  // Add the metadata Thing to the dataset
- dataset = setThing(dataset, updatedMetadataThing);
+ dataset = setThing(dataset, updatedMetadataThing1);
 
  // Save the dataset to your Solid pod
  await saveSolidDatasetAt(metadataUrl, dataset, { fetch });
 }
 
 
+async function uploadImageToPod(imageFile:File, targetUrl:string) {
+ // Read the image file as an ArrayBuffer
+ const imageArrayBuffer = await new Promise((resolve, reject) => {
+  const fileReader = new FileReader();
+  fileReader.onload = () => resolve(fileReader.result);
+  fileReader.onerror = reject;
+  fileReader.readAsArrayBuffer(imageFile);
+ });
+
+ // Upload the image to your Solid pod
+ try {
+  const savedFile = await overwriteFile(
+    targetUrl, // URL for the file.
+    new Blob([imageArrayBuffer], { type: imageFile.type }), // Blob containing file data
+    { fetch } // Fetch from the authenticated session
+  );
+  console.log(`File saved at ${getSourceUrl(savedFile)}`);
+ } catch (error) {
+  console.error(error);
+ }
+}
 
 async function handleFiles() {
 
  // Set the target URLs for the image and metadata files
- const imageTargetUrl = `${webId}/image${selectedFile.value}.jpg`;
- const metadataTargetUrl = `${webId}/metadata${selectedFile.value}.ttl`;
+ const imageTargetUrl = `${webId.split('/profile')[0]}/private/images/image/${selectedFile.value!.name}.jpg`.replace(/\s/g, '');
+ const metadataTargetUrl = `${webId.split('/profile')[0]}/private/images/metadata/${selectedFile.value!.name}.ttl`.replace(/\s/g, '');
+
+
 
  // Set the metadata
  const metadata = {
@@ -185,7 +276,7 @@ async function handleFiles() {
 
  // Upload the image and metadata files
  await uploadImageToPod(imageFile, imageTargetUrl);
- await uploadMetadata(metadataTargetUrl, metadata);
+ await uploadMetadata(metadataTargetUrl, metadata,imageTargetUrl);
 
  console.log('Image and metadata uploaded successfully.');
 }
@@ -194,25 +285,38 @@ async function handleFiles() {
 
 onMounted(async () => {
  contacts.value = await getContacts();
+ authorlist.value  = [... contacts.value];
+ authorlist.value.push({webId:webId, name: await getName(), image: await getProfilePicture()});
 });
 
 </script>
 
 <template>
  <h1>Add new post</h1>
+ <form @submit.prevent="handleFiles">
  <div class="postimg">
   <h2>Select post image</h2>
-  <input id="fileinput" type="file" accept="image/*" @change="handleFileChange">
-  <button @click="uploadImage">Upload Image</button>
-  <img :src="imageUrl" v-if="imageUrl">
+  <input id="fileinput" type="file" accept="image/*" @change="handleFileChange" required>
+  <img :src="imageUrl" v-if="imageUrl" class="image">
  </div>
+  <div class="postauthor">
+   <h2>Author</h2>
+   <div class="authorcontainer">
+   <div class="user" v-for="contact in authorlist">
+    <input type="radio" :value="contact.webId" v-model="postAuthor" required>
+    <img :src="contact.image" alt="no picture" class="contactPicture"/>
+    <label>{{contact.name? contact.name:"no foaf:name defined in profile" }}</label>
+
+   </div>
+   </div>
+  </div>
  <div class="posttitel">
   <h2>Title of your post</h2>
-  <input v-model="postTitle" type="text">
+  <input v-model="postTitle" type="text" placeholder="title" required >
  </div>
  <div class="postdescription">
   <h2>Description</h2>
-  <textarea v-model="postDescription"/>
+  <textarea v-model="postDescription" placeholder="description" required/>
  </div>
  <div class="tagged">
   <h2>Tagged people</h2>
@@ -226,10 +330,24 @@ onMounted(async () => {
     <td class="contactField"><input type="checkbox" :id="contact.webId" :value="contact.webId" v-model="tagged"></td>
    </tr>
   </div>
+  <input type="submit" value="Post">
  </div>
+ </form>
+ <RouterLink to="/feed"><button onclick="">Cancel</button></RouterLink>
 </template>
 
 <style scoped>
+ .image {
+  border-radius: 10px;
+  width: 50%;
+  height: auto;
+ }
+ .postimg {
+  display: flex;
+  flex-direction: column;
+  align-items: start;
+ }
+
  input {
   border-radius: 20px;
   border: 1px solid #00bd7e;
@@ -238,5 +356,18 @@ onMounted(async () => {
   padding: 5px;
   margin: 10px;
   color: white;
+ }
+ .user{
+  width: fit-content;
+  padding: 10px;
+
+ }
+ .authorcontainer{
+  display:grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 5px;
+  margin: 10px;
+  padding: 10px;
+  width: 100%;
  }
 </style>
--- src/views/PostFeed.vue
+++ src/views/PostFeed.vue
@@ -1,18 +1,20 @@
 <script setup lang="ts">
 import type { Contact } from "@/views/HomeView.vue";
-import { ref, type Ref } from "vue";
+import  type { Metadata } from "@/views/AddPost.vue";
+import { onMounted, ref, type Ref } from "vue";
 import { getDefaultSession } from "@inrupt/solid-client-authn-browser";
 import type * as RDF from "@rdfjs/types";
 import { QueryEngine } from "@comunica/query-sparql";
-import { getSolidDataset, getThingAll, getUrl } from "@inrupt/solid-client";
+import {
+  getContainedResourceUrlAll, getFile,
+  getSolidDataset,
+  getStringNoLocale,
+  getThing,
+  getThingAll,
+  getUrl,
+  getUrlAll
+} from "@inrupt/solid-client";
 import { FOAF } from "@inrupt/vocab-common-rdf";
-
-type Post = {
-  title: string | null,
-  description: string | null,
-  author: string | null,
-  tagged: [Contact] | null,
-}
 
 const session = getDefaultSession()
 const webId = session.info.webId;
@@ -24,19 +26,72 @@ let bindings: Ref<RDF.Bindings[]> = ref([])
 let resourceURL = webId
 const engine = new QueryEngine();
 
-let posts = ref<Array<Post>>([]);
+let posts = ref<Array<Metadata>>([]);
+let currentPost = ref<Metadata>({});
 
-async function getPosts() {
-  const profileUrl = resourceURL;
+async function fetchMetadataFiles() {
+  const metadataContainerUrl = `${resourceURL!.split('/profile')[0]}/private/images/metadata/`;
+  console.log("metadata container url "+metadataContainerUrl)
+  try {
+    const metadataContainerDataset = await getSolidDataset(metadataContainerUrl, { fetch });
+    console.log("metadata container dataset "+metadataContainerDataset)
+    // Get all metadata file URLs
+    const metadataFileUrls = getContainedResourceUrlAll(metadataContainerDataset);
+    console.log("metadata file urls "+metadataFileUrls)
+    // Fetch metadata files and process them
+    const metadataList = [];
+    for (const metadataFileUrl of metadataFileUrls) {
+      console.log("file url "+metadataFileUrl);
+      const metadataDataset = await getSolidDataset(metadataFileUrl, { fetch });
+      console.log("metadata dataset "+metadataDataset)
+      const metadataThing = getThingAll(metadataDataset)[0];
+      console.log("metadata thing "+metadataThing)
+      if (!metadataThing) {
+        throw new Error(`No metadata found in ${metadataFileUrl}`);
+      }
+      const metadata = {
+        file: await fetchPrivateImage(getUrl(metadataThing, "http://xmlns.com/foaf/0.1/img")),
+        title: getStringNoLocale(metadataThing, "http://xmlns.com/foaf/0.1/title"),
+        description: getStringNoLocale(metadataThing, "http://xmlns.com/foaf/0.1/depiction"),
+        tagged: getUrlAll(metadataThing, "http://xmlns.com/foaf/0.1/depicts"),
+        author: getUrl(metadataThing, "http://xmlns.com/foaf/0.1/maker")
+      };
 
-  const profileDataset = await getSolidDataset(profileUrl, { fetch });
+      metadataList.push(metadata);
+      console.log("metadata succesfully retrieved"+metadata.file)
+    }
 
-  // Get all Things in the dataset
-  const allThings = getThingAll(profileDataset);
-
-  //const posts = allThings.filter(thing => getUrl(thing, post))
+    return metadataList;
+  } catch (error) {
+    // If an error occurs (e.g., the path does not exist), return an empty list
+    console.error("Error fetching metadata files:", error);
+    return [];
+  }
 }
 
+async function fetchPrivateImage(imageUrl:string) {
+  try {
+    // Fetch the image file from the Solid POD
+    const imageFile = await getFile(imageUrl, { fetch });
+
+    // Convert the fetched image to a Blob URL
+    const blobUrl = URL.createObjectURL(imageFile);
+
+    return blobUrl;
+  } catch (error) {
+    console.error("Error fetching private image:", error);
+    return null;
+  }
+}
+
+
+async function changeCurrentPost(post: Metadata){
+  currentPost.value = post;
+}
+
+onMounted(async () => {
+  posts.value = await fetchMetadataFiles();
+})
 </script>
 
 <template>
@@ -46,16 +101,48 @@ async function getPosts() {
     </div>
     <div class="postfeed">
       <h1 class="feedHeader">Your Posts</h1>
-      <div v-if="posts===[]">
+      <div v-if="posts.length===0">
         <p>You currently have no posts</p>
       </div>
       <div v-else class="verticalContainer postList">
-        <p>TODO: display all images</p>
+        <div class="rowdiv">
+          <div class="leftside">
+            <div class="verticalcontainer">
+              <tr v-for="post in posts" :key="post.title">
+                <td class="contactField" @click="changeCurrentPost(post)"><img :src="post.file" alt="no picture" class="postPicture"/></td>
+              </tr>
+            </div>
+          </div>
+          <div class="rightside">
+            <h1>Post details</h1>
+            <div v-if="!currentPost.file">
+              <p>Click on a post to view details</p>
+            </div>
+            <div v-else>
+              <img :src="currentPost.file" alt="no picture" class="detailPicture">
+              <h2 class="postDetails">Title: {{currentPost.title}}</h2>
+              <h2 class="postDetails">Description: </h2>
+              <p>{{currentPost.description}}</p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </main>
 </template>
 
 <style scoped>
+
+.postPicture{
+    width: 50%;
+    height: auto;
+    border-radius: 10px;
+}
+
+.detailPicture{
+    width: 100%;
+    height: auto;
+    border-radius: 10px;
+}
 
 </style>
